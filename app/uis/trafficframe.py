97fe485ea01e209e2885d56bcde90ffc4a7687b4
lyt-cloud <1737402827@qq.com>
2021-03-14 22:35:40 +0800

    day01

--- app/uis/trafficframe.py
+++ app/uis/trafficframe.py
@@ -1,5 +1,10 @@
 # 主窗口
 from PyQt5.QtWidgets import QDialog
+
+from app.uis.trafficui import Ui_Dialog
+
 class TrafficFrame(QDialog):
     def __init__(self):
         super(TrafficFrame,self).__init__()
+        self.ui=Ui_Dialog()
+        self.ui.setupUi(self)
